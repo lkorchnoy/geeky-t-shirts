bbfc56d23628737d41cec2746f0a27b1b5c07e10
lkorchnoy <lkorchnoy@yahoo.com>
2021-03-04 17:43:56 -0500

    added focus file

---
+++ src/hooks/Focus.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+
+export function useFocus(ref: React.RefObject<HTMLInputElement>) {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (ref?.current && window.scrollY < 200) {
+        ref?.current?.focus();
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [ref]);
